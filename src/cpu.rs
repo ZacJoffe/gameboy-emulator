519bc3bc2b4014d9b5446398f57858954914514f
Zac Joffe <zacharyjoffe@gmail.com>
2019-12-28 15:54:28 -0500

    Flag registers

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,10 +1,46 @@
+use std::convert::From;
+
+struct Flags {
+    zero: bool,
+    subtract: bool,
+    half_carry: bool,
+    carry: bool
+}
+
+impl From<Flags> for u8 {
+    fn from(flag: Flags) -> u8 {
+        let zero = if flag.zero { 1 } else { 0 };
+        let subtract = if flag.subtract { 1 } else { 0 };
+        let half_carry = if flag.half_carry { 1 } else { 0 };
+        let carry = if flag.carry { 1 } else { 0 };
+
+        (zero << 7) | (subtract << 6) | (half_carry << 5) | (carry << 4)
+    }
+}
+
+impl From<u8> for Flags {
+    fn from(byte: u8) -> Self {
+        let zero = ((byte >> 7) & 0b1) != 0;
+        let subtract = ((byte >> 6) & 0b1) != 0;
+        let half_carry = ((byte >> 5) & 0b1) != 0;
+        let carry = ((byte >> 4) & 0b1) != 0;
+
+        Flags {
+            zero,
+            subtract,
+            half_carry,
+            carry
+        }
+    }
+}
+
 struct Registers {
     a: u8,
     b: u8,
     c: u8,
     d: u8,
     e: u8,
-    f: u8,
+    f: Flags,
     h: u8,
     l: u8
 }
