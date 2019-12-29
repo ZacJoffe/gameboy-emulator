e6706c491fdddd107b5220b0266aed2e3eb51882
Zac Joffe <zacharyjoffe@gmail.com>
2019-12-29 01:21:20 -0500

    Most code for add instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -4,7 +4,7 @@ struct Flags {
     zero: bool,
     subtract: bool,
     half_carry: bool,
-    carry: bool
+    pub carry: bool
 }
 
 impl Flags {
@@ -125,36 +125,100 @@ impl Registers {
 
 
 struct CPU {
+    pc: u16,
+    sp: u16,
     registers: Registers,
 }
 
+
 enum Instruction {
-    ADD(ArithTarget)
+    ADD(ArithTarget),
+    ADC(ArithTarget),
+    ADDHL(AddHLTarget)
 }
 
 enum ArithTarget {
-    A, B, C, D, E, H, L
+    A, B, C, D, E, H, L, D8, HLI
+}
+
+enum AddHLTarget {
+    BC, DE, HL, SP
 }
 
 impl CPU {
     fn execute(&mut self, instr: Instruction) {
         match instr {
             Instruction::ADD(target) => {
-                match target {
-                    ArithTarget::C => {
-                        // add c to a
-                        let value = self.registers.c;
-                        let result = self.add(value);
-                        self.registers.a = result;
-                    }
-                    _ => {}
-                }
+                self.add(target);
+            }
+            Instruction::ADC(target) => {
+                // do an ADD, then add the carry
+                self.add(target);
+                self.add_a(self.registers.f.carry as u8);
+            }
+            Instruction::ADDHL(target) => {
+                let value = match target {
+                    AddHLTarget::BC => { self.registers.get_bc() },
+                    AddHLTarget::DE => { self.registers.get_de() },
+                    AddHLTarget::HL => { self.registers.get_hl() },
+                    AddHLTarget::SP => { self.sp },
+                };
+                let result = self.add_hl(value);
+                self.registers.set_hl(result);
             }
             _ => {}
         }
     }
 
-    fn add(&mut self, value: u8) -> u8 {
+    // ADD instruction
+    fn add(&mut self, target: ArithTarget) {
+        match target {
+            ArithTarget::HLI => {
+
+            },
+            ArithTarget::D8 => {
+
+            },
+            ArithTarget::A => {
+                let value = self.registers.a;
+                let result = self.add_a(value);
+                self.registers.a = result;
+            },
+            ArithTarget::B => {
+                let value = self.registers.b;
+                let result = self.add_a(value);
+                self.registers.a = result;
+            },
+            ArithTarget::C => {
+                let value = self.registers.c;
+                let result = self.add_a(value);
+                self.registers.a = result;
+            },
+            ArithTarget::D => {
+                let value = self.registers.d;
+                let result = self.add_a(value);
+                self.registers.a = result;
+            },
+            ArithTarget::E => {
+                let value = self.registers.e;
+                let result = self.add_a(value);
+                self.registers.a = result;
+            },
+            ArithTarget::H => {
+                let value = self.registers.h;
+                let result = self.add_a(value);
+                self.registers.a = result;
+            },
+            ArithTarget::L => {
+                let value = self.registers.l;
+                let result = self.add_a(value);
+                self.registers.a = result;
+            }
+        }
+    }
+
+    // add to register a and set flags accordingly
+    fn add_a(&mut self, value: u8) -> u8 {
         let (result, did_overflow) = self.registers.a.overflowing_add(value);
 
         // set zero flag if the result is equal to 0
@@ -168,6 +232,25 @@ impl CPU {
 
         // set the half_carry flag if there was a carry to the upper nibble of a
         self.registers.f.half_carry = (self.registers.a & 0xf) + (value & 0xf) > 0xf;
+
+        // return the result of the addition
+        result
+    }
+
+    // add to registers hl and set flags accordingly
+    fn add_hl(&mut self, value: u16) -> u16 {
+        let (result, did_overflow) = self.registers.get_hl().overflowing_add(value);
+
+        // zero flag not set
+
+        // set subtract flag to false as this operation is an addition
+        self.registers.f.subtract = false;
+
+        // set carry flag if there was an overflow
+        self.registers.f.carry = did_overflow;
+
+        // set the half_carry flag if there was a carry to the upper nibble of hl
+        self.registers.f.half_carry = (self.registers.get_hl() & 0xfff) + (value & 0xfff) > 0xfff;
 
         // return the result of the addition
         result
